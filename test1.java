d330ca1efb48c16cac4044a7909591420953fe98
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-04-25 23:46:13 -0400

    completed work 456

--- test1.java
+++ test1.java
@@ -1,26 +1,29 @@
-public  class  test1 {
-    public  void  m1(){
+public class test1 {
+   public void m1() {
       System.out.println("hello");
    }
-   public  void  m2(){
+
+   public void m2() {
       System.out.println("m2");
    }
-     public  void  m3(){
+
+   public void m3() {
       System.out.println("m23");
    }
 
-   public  void m5(){
-     System.out.println("m5() method completed ");
+   public void m5() {
+      System.out.println("m5() method completed ");
    }
-   public void  m4(){
-    //completed
-    //123  456
-    //completed  
-  }
 
+   public void m4() {
+      // completed
+      // 123 456
+      // completed
+   }
 
- }
-   
+   public void m456() {
+      System.out.println("Exected method 456");
+      System.out.println("Sucess");
+   }
 
-
- 
+}
